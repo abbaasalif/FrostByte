40b53ffa13084308d298f9a9dcd6f38262211e6d
abbaasalif <abbaasalif@yahoo.com>
2022-01-15 00:42:32 -0500

    db updates

--- unenroll.py
+++ unenroll.py
@@ -26,11 +26,11 @@ studentTable = db.table("student")
 
 # retrieve the student document from the database, mark the student 
 # as unenrolled, and write back the document to the database
-student = studentTable.search(where(args["id"]))
-student[0][args["id"]][1] = "unenrolled"
-studentTable.write_back(student)
+# student = studentTable.search(where(args["id"]))
+# student[0][args["id"]][1] = "unenrolled"
+# studentTable.write_back(student)
 #to remove the record from the database
-#db.remove(where('id') == args["id"])
+db.remove(where('id') == args["id"])
 # delete the student's data from the dataset
 shutil.rmtree(os.path.join(conf["dataset_path"], conf["class"],
 	args["id"]))
