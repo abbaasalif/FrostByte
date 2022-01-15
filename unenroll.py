f24636d59b2b15ec2d3696d4ed1be730d00ce8f6
abbaasalif <abbaasalif@yahoo.com>
2022-01-15 01:16:07 -0500

    bla bla

--- unenroll.py
+++ unenroll.py
@@ -29,7 +29,7 @@ studentTable = db.table("student")
 student = studentTable.get(where(args["id"]))
 # studentTable.write_back(student)
 #to remove the record from the database
-val = student.keys()
+val = list(student.keys())
 #val = val[0]
 print(val)
 # delete the student's data from the dataset
